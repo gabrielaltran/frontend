fc298330b0f7270e98ad1fab9dded64856138f86
Gabriel Altran <altran.gabriel@gmail.com>
2024-07-12 15:16:39 -0300

    commit aula2 . Está dando erro no vite por alguma forma

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { CreateTripPage } from "./pages/create-trip";
-import { TripDetailsPage } from "./pages/trip-details";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { CreateTripPage } from "./pages/create-trip"
+import { TripDetailsPage } from "./pages/trip-details"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <CreateTripPage />,
+    element: <CreateTripPage />
   },
 	{
     path: "/trips/:tripId",
-    element: <TripDetailsPage />,
+    element: <TripDetailsPage />
   },
-]);
+])
 
 export function App() {
-  <RouterProvider router={router} />
+  return <RouterProvider router={router} />
 }
